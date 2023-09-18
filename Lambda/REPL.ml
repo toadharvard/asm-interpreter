Kakadu/fp2023

Add Lambda template
Signed-off-by: Kakadu <Kakadu@pm.me>

Lambda/REPL.ml
@@ -1,0 +1,56 @@
+open Base
+open Lambda_lib
+
+let run_repl _ =
+  Caml.Format.eprintf "OCaml-style toplevel (ocamlc, utop) is not implemented"
+;;
+
+let run_single eval =
+  let open Lambda_lib in
+  let text = Stdio.In_channel.(input_all stdin) |> String.rstrip in
+  let ast = Parser.parse text in
+  match ast with
+  | Error e -> Caml.Format.printf "Error: %a\n%!" Parser.pp_error e
+  | Result.Ok ast ->
+    Caml.Format.printf "Parsed result: %a\n%!" Printast.pp_named ast;
+    (match eval ast with
+     | rez -> Caml.Format.printf "Evaluated result: %a\n%!" Printast.pp_named rez)
+;;
+
+type strategy =
+  | CBN
+  | CBV
+  | NO
+  | AO
+
+type opts =
+  { mutable batch : bool
+  ; mutable stra : strategy
+  }
+
+let () =
+  let opts = { batch = false; stra = CBN } in
+  let open Caml.Arg in
+  parse
+    [ ( "-"
+      , Unit (fun () -> opts.batch <- true)
+      , "Read from stdin single program, instead of running full REPL" )
+    ; "-cbv", Unit (fun () -> opts.stra <- CBV), "Call-by-value strategy"
+    ; "-cbn", Unit (fun () -> opts.stra <- CBN), "Call-by-name strategy"
+    ; "-no", Unit (fun () -> opts.stra <- NO), "Normal Order strategy"
+    ; "-ao", Unit (fun () -> opts.stra <- NO), "Applicative Order strategy"
+    ]
+    (fun _ ->
+      Caml.Format.eprintf "Positioned arguments are not supported\n";
+      Caml.exit 1)
+    "Read-Eval-Print-Loop for Utyped Lambda Calculus";
+  let eval =
+    Lambda.apply_strat
+      (match opts.stra with
+       | NO -> Lambda.nor_strat
+       | CBV -> Lambda.cbv_strat
+       | AO -> Lambda.ao_strat
+       | CBN -> Lambda.cbn_strat)
+  in
+  (if opts.batch then run_single else run_repl) eval
+;;

Lambda/demos/demoParse.ml
@@ -1,0 +1,12 @@
+(** Copyright 2021-2022, Kakadu and contributors *)
+
+(** SPDX-License-Identifier: CC0-1.0 *)
+
+open Lambda_lib
+
+let () =
+  let s = Stdio.In_channel.input_all Caml.stdin in
+  match Lambda_lib.Parser.parse s with
+  | Result.Ok ast -> Format.printf "%a\n%!" Printast.pp_named ast
+  | Error _ -> Format.printf "Some error"
+;;

Lambda/lib/interpret.ml
@@ -1,0 +1,29 @@
+(** Copyright 2021-2022, Kakadu and contributors *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+(** Real monadic interpreter goes here *)
+
+open Base
+open Utils
+
+module Interpret (M : MONAD_FAIL) : sig
+  val run : _ Ast.t -> (int, Utils.error) M.t
+end = struct
+  let run _ =
+    (* implement interpreter here *)
+    if true then M.fail (UnknownVariable "var") else failwith "not implemented"
+  ;;
+end
+
+let parse_and_run str =
+  let ans =
+    let module I = Interpret (Result) in
+    match Parser.parse str with
+    | Caml.Result.Ok ast -> I.run ast
+    | Caml.Result.Error _ ->
+      Caml.Format.eprintf "Parsing error\n%!";
+      Caml.exit 1
+  in
+  ans
+;;

toadharvard/asm-interpreter

Signed-off-by: Dmitrii.Kosarev a.k.a. Kakadu Dmitrii.Kosarev@pm.me <Kakadu@pm.me>

Lambda/REPL.ml
@@ -1,0 +1,56 @@
+open Base
+open Lambda_lib
+
+let run_repl _ =
+  Stdlib.Format.eprintf "OCaml-style toplevel (ocamlc, utop) is not implemented"
+;;
+
+let run_single eval =
+  let open Lambda_lib in
+  let text = Stdio.In_channel.(input_all stdin) |> String.rstrip in
+  let ast = Parser.parse text in
+  match ast with
+  | Error e -> Stdlib.Format.printf "Error: %a\n%!" Parser.pp_error e
+  | Result.Ok ast ->
+    Stdlib.Format.printf "Parsed result: %a\n%!" Printast.pp_named ast;
+    (match eval ast with
+     | rez -> Stdlib.Format.printf "Evaluated result: %a\n%!" Printast.pp_named rez)
+;;
+
+type strategy =
+  | CBN
+  | CBV
+  | NO
+  | AO
+
+type opts =
+  { mutable batch : bool
+  ; mutable stra : strategy
+  }
+
+let () =
+  let opts = { batch = false; stra = CBN } in
+  let open Stdlib.Arg in
+  parse
+    [ ( "-"
+      , Unit (fun () -> opts.batch <- true)
+      , "Read from stdin single program, instead of running full REPL" )
+    ; "-cbv", Unit (fun () -> opts.stra <- CBV), "Call-by-value strategy"
+    ; "-cbn", Unit (fun () -> opts.stra <- CBN), "Call-by-name strategy"
+    ; "-no", Unit (fun () -> opts.stra <- NO), "Normal Order strategy"
+    ; "-ao", Unit (fun () -> opts.stra <- NO), "Applicative Order strategy"
+    ]
+    (fun _ ->
+      Stdlib.Format.eprintf "Positioned arguments are not supported\n";
+      Stdlib.exit 1)
+    "Read-Eval-Print-Loop for Utyped Lambda Calculus";
+  let eval =
+    Lambda.apply_strat
+      (match opts.stra with
+       | NO -> Lambda.nor_strat
+       | CBV -> Lambda.cbv_strat
+       | AO -> Lambda.ao_strat
+       | CBN -> Lambda.cbn_strat)
+  in
+  (if opts.batch then run_single else run_repl) eval
+;;

Lambda/demos/demoParse.ml
@@ -1,0 +1,12 @@
+(** Copyright 2021-2022, Kakadu and contributors *)
+
+(** SPDX-License-Identifier: CC0-1.0 *)
+
+open Lambda_lib
+
+let () =
+  let s = Stdio.In_channel.input_all Stdlib.stdin in
+  match Lambda_lib.Parser.parse s with
+  | Result.Ok ast -> Format.printf "%a\n%!" Printast.pp_named ast
+  | Error _ -> Format.printf "Some error"
+;;

Lambda/lib/interpret.ml
@@ -1,0 +1,29 @@
+(** Copyright 2021-2022, Kakadu and contributors *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+(** Real monadic interpreter goes here *)
+
+open Base
+open Utils
+
+module Interpret (M : MONAD_FAIL) : sig
+  val run : _ Ast.t -> (int, Utils.error) M.t
+end = struct
+  let run _ =
+    (* implement interpreter here *)
+    if true then M.fail (UnknownVariable "var") else failwith "not implemented"
+  ;;
+end
+
+let parse_and_run str =
+  let ans =
+    let module I = Interpret (Result) in
+    match Parser.parse str with
+    | Stdlib.Result.Ok ast -> I.run ast
+    | Stdlib.Result.Error _ ->
+      Stdlib.Format.eprintf "Parsing error\n%!";
+      Stdlib.exit 1
+  in
+  ans
+;;