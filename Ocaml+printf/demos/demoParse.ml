cc25e605a4ad5eb0eefec3564989479ca51c2258
Artem Demchenko <artdem243@gmail.com>
2023-09-19 18:19:19 +0300

    copy-paste Lambda directory

--- Ocaml+printf/demos/demoParse.ml
+++ Ocaml+printf/demos/demoParse.ml
@@ -1,12 +1,12 @@
-(** Copyright 2021-2023, aartdem *)
+(** Copyright 2021-2022, Kakadu and contributors *)
 
-(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+(** SPDX-License-Identifier: CC0-1.0 *)
 
-open Ocaml_printf_lib
+open Lambda_lib
 
 let () =
   let s = Stdio.In_channel.input_all Stdlib.stdin in
-  match Ocaml_printf_lib.Parser.parse s with
-  | Result.Ok ast -> Format.printf "%a\n%!" Ast.pp_program ast
-  | Error msg -> Format.printf "Error %s\n" msg
+  match Lambda_lib.Parser.parse s with
+  | Result.Ok ast -> Format.printf "%a\n%!" Printast.pp_named ast
+  | Error _ -> Format.printf "Some error"
 ;;
