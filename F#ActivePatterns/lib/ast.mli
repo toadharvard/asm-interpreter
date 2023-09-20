fb7f9c0382d9978625b13bae73edc253c13ed221
Vitaliy <kykyky228kykyk@gmail.com>
2023-09-21 01:47:05 +0300

    Choose F#ActivePatterns
    Signed-off-by: Vitaliy <kykyky228kykyk@gmail.com>

---
+++ F#ActivePatterns/lib/ast.mli
@@ -0,0 +1,15 @@
+(** Copyright 2021-2023, Kakadu and contributors *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+type name = string
+
+(** The main type for our AST (дерева абстрактного синтаксиса) *)
+type 'name t =
+  | Var of 'name (** Variable [x] *)
+  | Abs of 'name * 'name t (** Abstraction [λx.t] *)
+  | App of 'name t * 'name t
+
+(* Application [f g ] *)
+(** In type definition above the 3rd constructor is intentionally without documentation
+to test linter *)
