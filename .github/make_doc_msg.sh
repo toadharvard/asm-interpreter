0b7c9ce5bb6ef6be283301a886abe39c4ccd71cc
Dmitrii.Kosarev a.k.a. Kakadu Dmitrii.Kosarev@pm.me <Kakadu@pm.me>
2023-10-17 23:58:55 +0300

    Port new CI changes
    Signed-off-by: Dmitrii.Kosarev a.k.a. Kakadu Dmitrii.Kosarev@pm.me <Kakadu@pm.me>

--- .github/make_doc_msg.sh
+++ .github/make_doc_msg.sh
@@ -5,4 +5,4 @@ LANGNAME=$2
 OUTFILE=$3
 DATE=$(TZ='Europe/Moscow' date +%F\ %k:%M)
 
-echo "[Документация](https://kakadu.github.io/$REPONAME/docs/$LANGNAME) и [тестовое покрытие](https://kakadu.github.io/$REPONAME/cov/$LANGNAME) $(cat _coverage/percent.txt) должны скоро появиться.\n\n $DATE" > $OUTFILE
+echo "Документация и тестовое покрытие $(cat _coverage/percent.txt) должны скоро появиться.\n\nhttps://kakadu.github.io/$REPONAME/docs/$LANGNAME\n\nhttps://kakadu.github.io/$REPONAME/cov/$LANGNAME\n\n$DATE" > $OUTFILE
