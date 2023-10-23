4b6485b3b1dd0aac0bf7bc0c3e295dfe8f3891b0
Artem Demchenko <artdem243@gmail.com>
2023-10-23 03:04:42 +0300

    add docs to ast and tests

--- Ocaml+printf/demos/demoParse.ml
+++ Ocaml+printf/demos/demoParse.ml
@@ -1,7 +1,1 @@
-(** Copyright 2021-2022, Kakadu and contributors *)
-
-(** SPDX-License-Identifier: CC0-1.0 *)
-
-(* open Ocaml_printf_lib *)
-
 let () = Format.printf "Tests are not emplemented"
--- Ocaml+printf/lib/tests.ml
+++ Ocaml+printf/lib/tests.ml
@@ -1,26 +1,19 @@
 open Angstrom
-open Ocaml_printf_lib.Parser
-open Ocaml_printf_lib.Ast
+open Parser
+open Ast
 
-(* simple tests *)
-let%test _ =
-  parse_string ~consume:All expr_valname_parser "_" = Ok (Expr_val (LCIdent "_"))
-;;
+(* simple unit tests *)
+let%test _ = parse_string ~consume:All expr_valname "_" = Ok (Expr_val (LCIdent "_"))
 
 let%test _ =
-  parse_string ~consume:All expr_valname_parser "_Aasd0320"
-  = Ok (Expr_val (LCIdent "_Aasd0320"))
+  parse_string ~consume:All expr_valname "_Aasd0320" = Ok (Expr_val (LCIdent "_Aasd0320"))
 ;;
 
-let%test _ =
-  parse_string ~consume:All expr_valname_parser "a" = Ok (Expr_val (LCIdent "a"))
-;;
+let%test _ = parse_string ~consume:All expr_valname "a" = Ok (Expr_val (LCIdent "a"))
+let%test _ = parse_string ~consume:All (parenthesis (char 'a')) "  (  a  )" = Ok 'a'
+let%test _ = parse_string ~consume:All const_integer "123" = Ok (Int 123)
 
-let%test _ = parse_string ~consume:All (parenthesis @@ char 'a') "  (  a  )" = Ok 'a'
-let%test _ = parse_string ~consume:All const_integer_parser "123" = Ok (Int 123)
-
-(* big tests *)
-
+(* helper function for inline testing *)
 let test_parser p input expected =
   let result = parse_string p ~consume:All input in
   match result with
@@ -34,13 +27,15 @@ let test_parser p input expected =
     false
 ;;
 
+(* tests for interpretator *)
+
 let%test _ =
   let expexted =
     [ Let_decl (false, LCIdent "_a", Expr_const (Int 1))
-    ; Let_decl (false, LCIdent "_b", Expr_const (Int 1))
+    ; Let_decl (false, LCIdent "_b", Bin_op (Add, Expr_const (Int 1), Expr_const (Int 2)))
     ]
   in
-  test_parser program_parser "  ;; ;;;;   ;; ;;let _a=1;; ;;;; let   _b =1  " expexted
+  test_parser program_parser "  ;; ;;;;   ;; ;;let _a=1;; ;;;; let   _b =1+2  " expexted
 ;;
 
 let%test _ =
@@ -72,18 +67,72 @@ let%test _ =
                 , Bin_op
                     ( Div
                     , Expr_const (Int 10)
-                    , Un_op (Un_plus, Bin_op (Add, Expr_const (Int 2), Expr_const (Int 5)))
-                    )
-                , Bin_op (Sub, Expr_const (Int 7), Un_op (Un_minus, Expr_const (Int 1)))
-                )
+                    , Bin_op (Add, Expr_const (Int 2), Expr_const (Int 5)) )
+                , Bin_op (Sub, Expr_const (Int 7), Expr_const (Int 1)) )
             , Bin_op
                 ( Add
                 , Expr_const (Int 1)
                 , Bin_op (Mul, Expr_const (Int 2), Expr_const (Int 3)) ) ) )
     ]
   in
+  test_parser program_parser "let a =  10/( 2+5 )*( 7  - 1) - ( 1 + 2*  3 ) " expexted
+;;
+
+let%test _ =
+  let expexted =
+    [ Let_decl
+        ( false
+        , LCIdent "x"
+        , ITE
+            ( Expr_const (Int 1)
+            , ITE
+                ( Expr_const (Int 2)
+                , ITE (Expr_const (Int 3), Expr_const (Int 4), Expr_const (Int 5))
+                , Expr_const (Int 6) )
+            , Expr_const (Int 7) ) )
+    ]
+  in
   test_parser
     program_parser
-    "let a=  10  /  + (2+5 )  *  ( 7  - -1) - (1 + 2 * 3)"
+    "let x = if 1 then if 2 then if 3 then 4 else 5 else 6 else 7"
     expexted
+;;
+
+(* factorail *)
+
+let%test _ =
+  let expected =
+    [ Let_decl
+        ( true
+        , LCIdent "fac"
+        , Fun
+            ( LCIdent "n"
+            , ITE
+                ( Bin_op (Leq, Expr_val (LCIdent "n"), Expr_const (Int 1))
+                , Expr_const (Int 1)
+                , Bin_op
+                    ( Mul
+                    , Expr_val (LCIdent "n")
+                    , App
+                        ( Expr_val (LCIdent "fac")
+                        , Bin_op (Sub, Expr_val (LCIdent "n"), Expr_const (Int 1)) ) ) )
+            ) )
+    ]
+  in
+  test_parser
+    program_parser
+    "let rec fac n = if n <= 1 then 1 else n * fac (n - 1)"
+    expected
+;;
+
+
+let%test _ =
+  let expected =
+    [ 
+    ]
+  in
+  test_parser
+    program_parser
+    "let fib n = if n <= 1 then 1 else fib (n - 1) + fib (n - 2)"
+    expected
 ;;
