4cdbc4da455dbab6020cadd2fb0d4d44a82a8e8f
Vadim Yakshigulov <toadharvard@gmail.com>
2023-12-31 18:32:32 +0300

    fix: replace unsupported function

--- asm/lib/parser/statement.ml
+++ asm/lib/parser/statement.ml
@@ -37,7 +37,7 @@ let parse_instruction =
     ; gen_instruction "movapd" [ instruction movapd <$> parse_reg_128_reg_128 ]
     ; gen_instruction "punpckhqdq" [ instruction punpckhqdq <$> parse_reg_128_reg_128 ]
     ; gen_instruction "addpd" [ instruction addpd <$> parse_reg_128_reg_128 ]
-    ; gen_instruction "pinsrq" [instruction pinsrq <$> parse_reg_128_reg_64_imm_a]
+    ; gen_instruction "pinsrq" [ instruction pinsrq <$> parse_reg_128_reg_64_imm_a ]
     ; gen_instruction "mulpd" [ instruction mulpd <$> parse_reg_128_reg_128 ]
     ]
 ;;
