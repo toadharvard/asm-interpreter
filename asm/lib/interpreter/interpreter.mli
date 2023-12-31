c8ceaa248bccf2ae332a4f49add44c1e68d532cd
Vadim Yakshigulov <toadharvard@gmail.com>
2023-12-31 19:14:32 +0300

    reformat: reformat mli

--- asm/lib/interpreter/interpreter.mli
+++ asm/lib/interpreter/interpreter.mli
@@ -2,6 +2,6 @@
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
-val eval_ast :
-  Ast.statement list -> (State.GlobalState.t, unit) State.StateErrorMonad.t
+val eval_ast : Ast.statement list -> (State.GlobalState.t, unit) State.StateErrorMonad.t
+
 module State = State
