f50bfdc667b7131874e0c19eb1dcbb885f0985d2
Vadim Yakshigulov <toadharvard@gmail.com>
2023-12-31 19:12:15 +0300

    feat: add mli interfaces

---
+++ asm/lib/interpreter/interpreter.mli
@@ -0,0 +1,7 @@
+(** Copyright 2023-2024, Vadim Yakshigulov *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+val eval_ast :
+  Ast.statement list -> (State.GlobalState.t, unit) State.StateErrorMonad.t
+module State = State
