55fccc27ba80ffd7159f574f7d7e400fd3d9b654
Vadim Yakshigulov <toadharvard@gmail.com>
2024-01-08 14:36:23 -0800

    refactor: remove unused 32 bit registers

--- asm/lib/parser/operand.ml
+++ asm/lib/parser/operand.ml
@@ -8,32 +8,22 @@ open Common
 open Register
 open Immediate
 
-let parse_reg_32 = reg_32 <$> parse_32_register
 let parse_reg_64 = reg_64 <$> parse_64_register
 let parse_reg_128 = reg_128 <$> parse_128_register
 let parse_imm_a = imm_a <$> parse_immidiate
 
-let parse_mem_32_mem_32 =
-  lift2 mem_32_mem_32 parse_32_register_ref (comma *> parse_32_register_ref)
-;;
 
 let parse_mem_64_mem_64 =
   lift2 mem_64_mem_64 parse_64_register_ref (comma *> parse_64_register_ref)
-;;
-
-let parse_reg_32_reg_32 =
-  lift2 reg_32_reg_32 parse_32_register (comma *> parse_32_register)
 ;;
 
 let parse_reg_64_reg_64 =
   lift2 reg_64_reg_64 parse_64_register (comma *> parse_64_register)
 ;;
 
-let parse_mem_32 = mem_32 <$> parse_32_register_ref
 let parse_mem_64 = mem_64 <$> parse_64_register_ref
 let parse_label_operand = label <$> label_name
 let parse_nothing = return nothing
-let parse_reg_32_imm_a = lift2 reg_32_imm_a parse_32_register (comma *> parse_immidiate)
 let parse_reg_64_imm_a = lift2 reg_64_imm_a parse_64_register (comma *> parse_immidiate)
 
 let parse_reg_128_reg_128 =
--- asm/lib/parser/operand.mli
+++ asm/lib/parser/operand.mli
@@ -2,19 +2,14 @@
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
-val parse_reg_32 : [ `Reg_32 ] Ast.operand Angstrom.t
 val parse_reg_64 : [ `Reg_64 ] Ast.operand Angstrom.t
 val parse_reg_128 : [ `Reg_128 ] Ast.operand Angstrom.t
 val parse_imm_a : [ `Imm_a ] Ast.operand Angstrom.t
-val parse_mem_32_mem_32 : [ `Mem_32_mem_32 ] Ast.operand Angstrom.t
 val parse_mem_64_mem_64 : [ `Mem_64_mem_64 ] Ast.operand Angstrom.t
-val parse_reg_32_reg_32 : [ `Reg_32_reg_32 ] Ast.operand Angstrom.t
 val parse_reg_64_reg_64 : [ `Reg_64_reg_64 ] Ast.operand Angstrom.t
-val parse_mem_32 : [ `Mem_32 ] Ast.operand Angstrom.t
 val parse_mem_64 : [ `Mem_64 ] Ast.operand Angstrom.t
 val parse_label_operand : [ `Label ] Ast.operand Angstrom.t
 val parse_nothing : [ `Nothing ] Ast.operand Angstrom.t
-val parse_reg_32_imm_a : [ `Reg_32_imm_a ] Ast.operand Angstrom.t
 val parse_reg_64_imm_a : [ `Reg_64_imm_a ] Ast.operand Angstrom.t
 val parse_reg_128_reg_128 : [ `Reg_128_reg_128 ] Ast.operand Angstrom.t
 val parse_reg_128_reg_64 : [ `Reg_128_reg_64 ] Ast.operand Angstrom.t
